399095d7b2788e2b2ac465658d19e0905f9878d8
Bruna Mello <brunacrespomello@gmail.com>
2022-01-28 15:49:25 +0000

    Node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,21 @@
+//const criate constant (you cant change)
+
+//const fs = require('fs');
+
+//coping file
+//fs.copyFileSync('file1.txt', 'file2.txt');
+
+var superheroes = require('superheroes');
+
+console.log(superheroes.random());
+
+const supervillains = require('supervillains');
+
+supervillains.all;
+
+console.log(supervillains.random());
+
+
+
+
+
