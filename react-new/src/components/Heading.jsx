47941628f5f4635939d837aa3f3184a6ee9bc60a
brunacrespomello <brunacrespomello@gmail.com>
2022-06-10 14:43:49 +0100

    React module 402. React Components Practice

--- react-new/src/components/Heading.jsx
+++ react-new/src/components/Heading.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+
 function Heading() {
-    return <h1>My Favorite Food</h1> ;
+    const hour = new Date().getHours();
+    var say;
+
+    const customStyles = {
+        color: "blue",
+    }
+
+    if (hour > 0 && hour < 12){
+        say = "Good Morning!";
+        customStyles.color = "red";
+    } else if (hour > 12 && hour < 18 ) {
+        say = "Good Afternoon!";
+        customStyles.color = "green";
+    } else if (hour > 18 && hour < 23){
+        say = "Good Evening!";
+        customStyles.color = "blue";
+    } else {
+        say = "Error";
+        customStyles.color = "red";
+    }
+
+
+    return <h1 className="heading" style={customStyles}>{say}</h1> ;
 }
 
 export default Heading;
