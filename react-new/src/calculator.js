10941d5f9c1fe2018492dbd18e7a70577dce6c94
brunacrespomello <brunacrespomello@gmail.com>
2022-06-10 15:50:42 +0100

    React module 404. Javascript ES6 Import, Export and Modules Practice

---
+++ react-new/src/calculator.js
@@ -0,0 +1,22 @@
+
+function add(n1, n2) {
+    let addResult = n1 + n2;
+    return addResult;
+}
+
+function subtract(n1, n2) {
+    let subResult = n1 - n2;
+    return subResult;
+}
+
+function multiply(n1, n2) {
+    let multiResult = n1 * n2;
+    return multiResult;
+}
+
+function divide(n1, n2) {
+    let divResult = n1 / n2;
+    return divResult;
+}
+
+export {add, subtract, multiply, divide};
--- react-new/src/components/List.jsx
+++ react-new/src/components/List.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 // import pi, {doublePi, triplePi} from "../math";
 import * as math from "../math";
+import {add, subtract, multiply, divide} from "../calculator";
 
 function List() {
-    return <ul>
-        <li>{math.default}</li>
-        <li>{math.doublePi()}</li>
-        <li>{math.triplePi()}</li>
-    </ul>
+    return <div>
+        <p>Pi Operations</p>
+        <ul>
+            <li>{math.default}</li>
+            <li>{math.doublePi()}</li>
+            <li>{math.triplePi()}</li>
+        </ul>
+
+        <p>Math Operations</p>
+        <ul>
+            <li>{add(1, 2)}</li>
+            <li>{subtract(7, 2)}</li>
+            <li>{multiply(3,2)}</li>
+            <li>{divide(5,2)}</li>
+        </ul>
+    </div>
 }
 
 export default List;
