fb9dcb993891e7dfca7b16ce6c0982182743286b
brunacrespomello <brunacrespomello@gmail.com>
2022-06-10 13:23:14 +0100

    React module 400. React Styling Practice

--- react-new/src/index.js
+++ react-new/src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const hour = new Date().getHours();
 const customStyles = {
-    color: "blue",
-    fontsize:"20px",
-    border: "1px solid black",
+    color: "yellow",
 }
 
-customStyles.color = "red";
+var say;
+
+if (hour > 0 && hour < 12){
+    say = "Good Morning!";
+    customStyles.color = "red";
+} else if (hour > 12 && hour < 18 ) {
+    say = "Good Afternoon!";
+    customStyles.color = "green";
+} else if (hour > 18 && hour < 23){
+    say = "Good Evening!";
+    customStyles.color = "blue";
+} else {
+    say = "Error";
+    customStyles.color = "red";
+}
 
 
 ReactDOM.render(
     <div>
-        <h1 style={customStyles}> Hello Bruna</h1>
+        <h1 style={customStyles} className="heading">{say}</h1>
+
 
     </div>,
     document.getElementById("root")
