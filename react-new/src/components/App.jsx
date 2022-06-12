9ca70db58f8135165e8c6a045cefaa4f4bd8a31f
brunacrespomello <brunacrespomello@gmail.com>
2022-06-12 20:53:05 +0100

    React module 410. React Props

--- react-new/src/components/App.jsx
+++ react-new/src/components/App.jsx
@@ -1,13 +1,22 @@
 import '../App.css';
-import Heading from "./Heading";
-import List from "./List";
+import Card from "./Card";
 
 function App() {
   return (
       <div>
-        <Heading />
-        <List />
+          <Card
+              name="Beyonce"
+              img="https://e3.365dm.com/18/12/768x432/skynews-beyonce-beyonce-jay-z_4507925.jpg?20181203185757"
+              tel="+123 456 789"
+              email="b@beyonce.com"
+          />
 
+          <Card
+              name="Jack Bauer"
+              img="https://s.yimg.com/os/creatr-uploaded-images/2021-11/f5f8f1f0-3e3e-11ec-bfee-29cb63bf1393"
+              tel="+123 456 789"
+              email="jb@jackbauer.com"
+          />
       </div>
 
   );
---
+++ react-new/src/components/HeadingProps.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HeadingProps() {
+
+    return <div>
+        <h1>My Contacts</h1>
+        <h2>Beyonce</h2>
+        <img src="https://e3.365dm.com/18/12/768x432/skynews-beyonce-beyonce-jay-z_4507925.jpg?20181203185757"/>
+        <p>+123 456 789</p>
+        <p>b@beyonce.com</p>
+
+        <h2>Jack Bauer</h2>
+        <img src="https://s.yimg.com/os/creatr-uploaded-images/2021-11/f5f8f1f0-3e3e-11ec-bfee-29cb63bf1393"/>
+        <p>+123 456 789</p>
+        <p>jb@jackbauer.com</p>
+    </div>
+}
+
+export default HeadingProps;
