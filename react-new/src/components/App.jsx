6bd140d524ddef42cadf549f83bf09e6b408790f
brunacrespomello <brunacrespomello@gmail.com>
2022-06-14 15:52:00 +0100

    React module 413. Mapping Data to Components

--- react-new/src/components/App.jsx
+++ react-new/src/components/App.jsx
@@ -3,6 +3,16 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+    return <Card
+        key={contact.id}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+    />
+}
+
 function App() {
 
 
@@ -11,26 +21,29 @@ function App() {
       <div>
           <h1>My Contacts</h1>
           <Avatar img="https://avatars.githubusercontent.com/u/38960731?v=4"/>
-          <Card
-              name={contacts[0].name}
-              img={contacts[0].imgURL}
-              tel={contacts[0].phone}
-              email={contacts[0].email}
-          />
+          {contacts.map(createCard)}
 
-          <Card
-              name={contacts[1].name}
-              img={contacts[1].imgURL}
-              tel={contacts[1].phone}
-              email={contacts[1].email}
-          />
 
-          <Card
-              name={contacts[2].name}
-              img={contacts[2].imgURL}
-              tel={contacts[2].phone}
-              email={contacts[2].email}
-          />
+          {/*<Card*/}
+          {/*    name={contacts[0].name}*/}
+          {/*    img={contacts[0].imgURL}*/}
+          {/*    tel={contacts[0].phone}*/}
+          {/*    email={contacts[0].email}*/}
+          {/*/>*/}
+
+          {/*<Card*/}
+          {/*    name={contacts[1].name}*/}
+          {/*    img={contacts[1].imgURL}*/}
+          {/*    tel={contacts[1].phone}*/}
+          {/*    email={contacts[1].email}*/}
+          {/*/>*/}
+
+          {/*<Card*/}
+          {/*    name={contacts[2].name}*/}
+          {/*    img={contacts[2].imgURL}*/}
+          {/*    tel={contacts[2].phone}*/}
+          {/*    email={contacts[2].email}*/}
+          {/*/>*/}
       </div>
 
   );
