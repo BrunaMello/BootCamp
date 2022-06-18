946e9d494c5bfe68f39fe00a6cbaf4f51b3ddcfc
brunacrespomello <brunacrespomello@gmail.com>
2022-06-18 17:09:26 +0100

    React Module - Kepper App 417. Keeper App Project - Part 2

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,17 +1,25 @@
-
-import '../App.css';
+import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../resource/notes";
+
+
 
 function App() {
-  return (
-    <div>
-      <Header />
-        <Note />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            {notes.map(noteItems => (
+                <Note
+                    key={noteItems.key}
+                    title={noteItems.title}
+                    content={noteItems.content}
+                />
+            ))}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
