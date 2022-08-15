309c5ebc897ddcea5e93fffe5e7c9ccd6207c62a
brunacrespomello <brunacrespomello@gmail.com>
2022-08-15 02:11:30 +0100

    428. Changing Complex State

--- react-new/src/components/ReactForms.jsx
+++ react-new/src/components/ReactForms.jsx
@@ -2,32 +2,66 @@ import React, {useState} from "react";
 
 function ReactForms() {
 
-    const [name, setName] = useState("");
-    const [headingText, setHandingText] = useState("");
+    const [fullName, setFullName] = useState({
+        fName: "",
+        lName: ""
+
+    });
+
+    // const [headingText, setHandingText] = useState("");
+
+    // function handleChange(event) {
+    //     console.log(event.target.value);
+    //     setName(event.target.value);
+    // }
+    //
+    // function handleClick(event) {
+    //     setHandingText(name);
+    //     event.preventDefault();
+    // }
 
     function handleChange(event) {
-        console.log(event.target.value);
-        setName(event.target.value);
-    }
+        const newValue = event.target.value;
+        const inputName = event.target.name;
 
-    function handleClick(event) {
-        setHandingText(name);
-
-        event.preventDefault();
+        setFullName(prevValue => {
+            if (inputName === "fName") {
+                return {
+                    fName: newValue,
+                    lName: prevValue.lName
+                };
+            } else if (inputName === "lName") {
+                return {
+                    fName: prevValue.fName,
+                    lName: newValue
+                };
+            }
+        });
     }
 
     return (
         <div>
-            <h1>Hello {headingText}</h1>
-            <form onSubmit={handleClick}>
+            <h1>Hello {fullName.fName} {fullName.lName}</h1>
+            <form>
                 <input
+                    name="fName"
                     onChange={handleChange}
                     type="text"
                     placeholder="What's your name"
-                    value={name}
+                    value={fullName.fName}
                 />
+                <br/>
+                <br/>
+                <input
+                    name="lName"
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="What's your last name"
+                    value={fullName.lName}
+                />
+                <br/>
+                <br/>
                 <button
-                    onClick={handleClick}
                     type="submit"
                 >Submit</button>
             </form>
--- react-new/src/index.js
+++ react-new/src/index.js
@@ -12,7 +12,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-    <ClassComponent />
+    <ReactForms />
 );
 
 // ReactDOM.render(
